68f25a7ff0fdfcdf170d6f6475f2376f3bd41915
magnuguli <magnus.gule@piscada.com>
2022-10-28 11:15:08 +0200

    vanilla-extract rollup plugin. This might work!

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,40 @@
-import resolve from "@rollup/plugin-node-resolve";
-import commonjs from "@rollup/plugin-commonjs";
-import typescript from "@rollup/plugin-typescript";
-import dts from "rollup-plugin-dts";
-import postcss from "rollup-plugin-postcss";
-import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import postcss from 'rollup-plugin-postcss';
+import excludeDependenciesFromBundle from 'rollup-plugin-exclude-dependencies-from-bundle';
+import { vanillaExtractPlugin } from '@vanilla-extract/rollup-plugin';
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
-          sourcemap: true,
+        format: 'esm',
+        sourcemap: true,
       },
     ],
     plugins: [
       excludeDependenciesFromBundle(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript({ tsconfig: './tsconfig.json' }),
       postcss(),
+      vanillaExtractPlugin(),
     ],
   },
   {
-    input: "dist/esm/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
     external: [/\.(css|less|sass|scss)$/],
   },
